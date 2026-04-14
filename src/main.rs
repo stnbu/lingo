d9fa69ee1b1d0195fccc9e219730a98d394ba320
Mike Burr <mb@unintuitive.org>
2026-04-13 19:52:37 -0600

    .

--- src/main.rs
+++ src/main.rs
@@ -10,23 +10,18 @@ fn main() -> Result<(), eframe::Error> {
         ..Default::default()
     };
 
-    // Our application state:
-    let mut name = "Arthur".to_owned();
-    let mut age = 42;
-
     eframe::run_ui_native("My egui App", options, move |ctx, _frame| {
         egui::CentralPanel::default().show_inside(ctx, |ui| {
             ui.heading("My egui Application");
-            ui.horizontal(|ui| {
-                let name_label = ui.label("Your name: ");
-                ui.text_edit_singleline(&mut name)
-                    .labelled_by(name_label.id);
-            });
-            ui.add(egui::Slider::new(&mut age, 0..=120).text("age"));
-            if ui.button("Click each year").clicked() {
-                age += 1;
+            if ui.button("Pass").clicked() {
+                println!("pass");
             }
-            ui.label(format!("Hello '{name}', age {age}"));
+            if ui.button("Fail").clicked() {
+                println!("fail");
+            }
+            if ui.button("Flip").clicked() {
+                println!("flip");
+            }
         });
     })
 }
