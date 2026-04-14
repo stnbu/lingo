1c977aa9246e5c340abbf8c7b8a615e3859a48dc
Mike Burr <mb@unintuitive.org>
2026-04-14 14:23:31 -0600

    .

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,43 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
 use eframe::egui;
+use rusqlite::{Connection, Result};
+
+pub struct Vocab {
+    pub id: i64,
+    pub vocab: String,
+    pub reading: String,
+    pub translation: String,
+}
+
+pub fn get_vocab_by_id(path: &str, id: i64) -> Result<Vocab> {
+    let conn = Connection::open(path)?;
+
+    let mut stmt = conn.prepare(
+        "SELECT id, vocab, reading, translation FROM vocab WHERE id = ?1"
+    )?;
+
+    let v = stmt.query_row([id], |row| {
+        Ok(Vocab {
+            id: row.get(0)?,
+            vocab: row.get(1)?,
+            reading: row.get(2)?,
+            translation: row.get(3)?,
+        })
+    })?;
+
+    Ok(v)
+}
+
+/*
+fn main() -> anyhow::Result<()> {
+    let db_path = "vocab.db";
+    // /Users/mburr/Downloads/vocab.db
+    let v = db::get_vocab_by_id(db_path, 1)?;
+    println!("{} {} {} {}", v.id, v.vocab, v.reading, v.translation);
+    Ok(())
+}
+*/
 
 fn main() -> Result<(), eframe::Error> {
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
